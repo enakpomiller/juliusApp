85c8ad75d90b81ca66e1ec9dad604a354cd3fc8b
enakpomiller <63649279+enakpomiller@users.noreply.github.com>
2023-08-07 12:46:58 +0100

    header creation

--- application/views/layout/fragment/header.php
+++ application/views/layout/fragment/header.php
@@ -1,2 +1,17 @@
 
-<h1> header </h1>
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+ <div class="header">
+    <div class="nav">
+        <a href="" class="txt">  Hone  </a>
+        <a href="" class="txt">  About Us </a>
+        <a href="" class="txt">  Contact Us  </a>
+        <a href="" class="txt">  Gallery  </a>
+   </div>
+</div>
+
